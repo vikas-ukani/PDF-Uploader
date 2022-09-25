d9db397b81c6cdba4b134fe8917cd874c519e168
Vikas Ukani <vikasukani5@gmail.com>
2022-09-25 19:57:32 +0530

    Done: User Authentication Completed.

---
+++ database/migrations/2022_09_25_112114_create_files_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 200)->comment('file name');
+            $table->string('path', 400)->comment('file path');
+            $table->string('type', 50)->comment('file type');
+
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+};
